fd39033a08e20ad7112588d02ad649ac51b63746
henry.fan <henry.fan@softtek.com>
2020-10-21 14:56:58 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,67 @@
 import {
     Wechaty,
-    log
+    log,
+    ScanStatus,
+    Contact,
+    Message
 } from 'wechaty';
+import {generate} from 'qrcode-terminal';
+import TaoBaoController from './controller/taobao';
 
-import WeChatController from './controller/wechat';
-
-let wecaht = new WeChatController();
+const taobao = new TaoBaoController();
 
 const bot = new Wechaty({
     name: 'wechat-fanli-robot',
-    puppet: 'wechaty-puppet-padplus'
+    puppet: 'wechaty-puppet-padplus',
+    puppetOptions: {}
 });
 
-bot.on('scan', wecaht.onScan);
-bot.on('login', wecaht.onLogin);
-bot.on('logout', wecaht.onLogout);
-bot.on('message', wecaht.onMessage);
+function onScan(qrcode: string, status: ScanStatus) {
+    if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
+        generate(qrcode, {small: true});  // show qrcode on console
+
+        const qrcodeImageUrl = [
+            'https://wechaty.js.org/qrcode/',
+            encodeURIComponent(qrcode),
+        ].join('');
+
+        log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl);
+    } else {
+        log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status);
+    }
+}
+
+function onLogin(user: Contact) {
+    log.info('StarterBot', '%s login', user);
+}
+
+function onLogout(user: Contact) {
+    log.info('StarterBot', '%s logout', user);
+}
+
+async function onMessage(msg: Message) {
+    log.info('StarterBot', msg.toString());
+
+    if (msg.text()) {
+        try {
+            const rgx = /([\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6])\w{8,12}([\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6])/;
+            const pwd: any = msg.text().match(rgx);
+            if (pwd !== null) {
+                const res = await taobao.getInfo(pwd[0]);
+                await msg.say(res);
+            } else if (msg.text() === 'ding') {
+                await msg.say('dong');
+            }
+        } catch (err) {
+            log.error(err);
+        }
+    }
+}
+
+bot.on('scan', onScan);
+bot.on('login', onLogin);
+bot.on('logout', onLogout);
+bot.on('message', onMessage);
 
 bot.start()
     .then(() => log.info('StarterBot', 'Starter Bot Started.'))
