fe729c15ec58033b9987af54eecd5f0bbbf40c8b
henry.fan <henry.fan@softtek.com>
2020-10-21 16:25:31 +0800

    update README

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import {
     log,
     ScanStatus,
     Contact,
-    Message
+    Message,
+    Friendship,
+
 } from 'wechaty';
 import {generate} from 'qrcode-terminal';
 import TaoBaoController from './controller/taobao';
@@ -39,11 +41,18 @@ function onLogout(user: Contact) {
     log.info('StarterBot', '%s logout', user);
 }
 
+async function onFriendship(friendship: any) {
+    if (friendship.type() === Friendship.Type.Receive) {
+        await friendship.accept();
+    }
+}
+
 async function onMessage(msg: Message) {
     log.info('StarterBot', msg.toString());
 
     if (msg.text()) {
         try {
+            // 提取淘口令
             const rgx = /([\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6])\w{8,12}([\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6])/;
             const pwd: any = msg.text().match(rgx);
             if (pwd !== null) {
@@ -62,7 +71,12 @@ bot.on('scan', onScan);
 bot.on('login', onLogin);
 bot.on('logout', onLogout);
 bot.on('message', onMessage);
+bot.on('friendship', onFriendship);
 
 bot.start()
     .then(() => log.info('StarterBot', 'Starter Bot Started.'))
     .catch(e => log.error('StarterBot', e));
+
+bot.on('error', (error) => {
+    log.error(error);
+});
