843bf45936a8dc7c5f75f867ac739d90bca92fa6
henry.fan <henry.fan@softtek.com>
2021-06-16 14:34:04 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,65 @@
 import {
-    Wechaty,
-    log,
-    ScanStatus,
-    Contact,
-    Message,
-    Friendship,
+  Wechaty,
+  log,
+  ScanStatus,
+  Contact,
+  Message,
+  Friendship,
 
 } from 'wechaty';
 import { generate } from 'qrcode-terminal';
-import TaoBaoController from './controller/taobao';
+import { template as tmp } from './lib/message-template';
+import Service from './service';
 
-const taobao = new TaoBaoController();
+const service = new Service();
 
 const bot = new Wechaty({
-    name: 'wechat-fanli-robot',
-    puppet: 'wechaty-puppet-wechat',
-    puppetOptions: {}
+  name: 'wechat-fanli-robot',
+  puppet: 'wechaty-puppet-wechat',
+  puppetOptions: {}
 });
 
 function onScan(qrcode: string, status: ScanStatus) {
-    if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
-        generate(qrcode, { small: true });  // show qrcode on console
+  if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
+    generate(qrcode, { small: true });  // show qrcode on console
 
-        const qrcodeImageUrl = [
-            'https://wechaty.js.org/qrcode/',
-            encodeURIComponent(qrcode),
-        ].join('');
+    const qrcodeImageUrl = [
+      'https://wechaty.js.org/qrcode/',
+      encodeURIComponent(qrcode),
+    ].join('');
 
-        log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl);
-    } else {
-        log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status);
-    }
+    log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl);
+  } else {
+    log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status);
+  }
 }
 
 function onLogin(user: Contact) {
-    log.info('StarterBot', '%s login', user);
+  log.info('StarterBot', '%s login', user);
 }
 
 function onLogout(user: Contact) {
-    log.info('StarterBot', '%s logout', user);
+  log.info('StarterBot', '%s logout', user);
 }
 
 async function onFriendship(friendship: any) {
-    if (friendship.type() === Friendship.Type.Receive) {
-        await friendship.accept();
-    }
+  if (friendship.type() === Friendship.Type.Receive) {
+    await friendship.accept();
+  }
 }
 
 async function onMessage(msg: Message) {
-    log.info('StarterBot', msg.toString());
-    try {
-        if (msg.text()) {
-            // 提取淘口令
-            // const rgx = /([\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6])\w{8,12}([\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6])/;
-            const rgx = /\w{8,11}/;
-            const pwd: any = msg.text().match(rgx);
-            if (pwd !== null) {
-                const res = await taobao.getInfo(`￥${pwd[0]}￥`);
-                await msg.say(res);
-            } else if (msg.text() === 'ding') {
-                await msg.say('dong');
-            }
-        }
-    } catch (err) {
-        log.error(err);
+  log.info('StarterBot', msg.toString());
+  try {
+    if (msg.text()) {
+      const text = msg.text();
+      await msg.say(tmp(text));
+      await service.send(text);
     }
+  } catch (err) {
+    log.error(err);
+    await msg.say('好像有什么不对呢');
+  }
 }
 
 bot.on('scan', onScan);
@@ -74,9 +69,9 @@ bot.on('message', onMessage);
 bot.on('friendship', onFriendship);
 
 bot.start()
-    .then(() => log.info('StarterBot', 'Starter Bot Started.'))
-    .catch(e => log.error('StarterBot', e));
+  .then(() => log.info('StarterBot', 'Starter Bot Started.'))
+  .catch(e => log.error('StarterBot', e));
 
 bot.on('error', (error) => {
-    log.error(error);
+  log.error(error);
 });
---
+++ src/service.ts
@@ -0,0 +1,16 @@
+import rp from 'request-promise';
+
+export default class Service {
+  async send(msg: string) {
+    const opt = {
+      method: 'POST',
+      uri: 'http://127.0.0.1:4000',
+      json: true,
+      strictSSL: false,
+      body: { msg },
+    };
+    return rp(opt).catch(err => {
+      return err;
+    });
+  }
+}
